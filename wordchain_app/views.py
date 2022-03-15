fafd95dd47c7a0136c18a0bbce921c9a7eabfc47
Diego <dh8cd@virginia.edu>
2022-03-15 15:58:21 -0400

    added css file

--- wordchain_app/views.py
+++ wordchain_app/views.py
@@ -4,19 +4,16 @@ from django.shortcuts import render
 
 @login_required
 def home(request):
-    return render(request, "home.html")
+    return render(request, "wordchain_app/home.html")
 
 def sign_up(request):
-    return render(request, "sign_up.html")
+    return render(request, "wordchain_app/sign_up.html")
 
 def password_reset(request):
-    return render(request, "password_reset.html")
+    return render(request, "wordchain_app/password_reset.html")
 
 def update_account_details(request):
-    return render(request, "update_account_details.html")
-
-# def home(request):
-#     return render(request, "home.html")
+    return render(request, "wordchain_app/update_account_details.html")
 
 def play(request):
-    return render(request, "play.html")
+    return render(request, "wordchain_app/play.html")
