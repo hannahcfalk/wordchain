c67ee289a84a57e49084c936e79cc76a42a9b4bd
hannahcfalk <hannahcfalk@gmail.com>
2022-03-23 20:30:20 -0400

    Initial commit

--- wordchain_app/views.py
+++ wordchain_app/views.py
@@ -1,13 +1,23 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
+from .forms import SignUpForm
 
 @login_required
 def home(request):
     return render(request, "wordchain_app/home.html")
 
+
 def sign_up(request):
-    return render(request, "wordchain_app/sign_up.html")
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('wordchain:home')
+
+    else:
+        form = SignUpForm()
+    return render(request, "registration/sign_up.html", {'form': form})
 
 def password_reset(request):
     return render(request, "wordchain_app/password_reset.html")
