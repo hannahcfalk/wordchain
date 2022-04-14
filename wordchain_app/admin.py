64ec3dcd4c47aa41584bd1635998b94b35fd79b7
Diego <dh8cd@virginia.edu>
2022-04-13 21:34:52 -0400

    diego wed-update

--- wordchain_app/admin.py
+++ wordchain_app/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Chain
 from .models import *
+from django.apps import apps
 
+class ModelAdmin(admin.ModelAdmin):
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields]
+        super().__init__(model, admin_site)
 
-admin.site.register(Chain)
-admin.site.register(Display)
-admin.site.register(SetView)
+models = apps.get_models()
+for model in models:
+    try:
+        admin.site.register(model, ModelAdmin)
+    except admin.sites.AlreadyRegistered:
+        pass
+
